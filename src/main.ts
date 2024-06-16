0562c1880ca5c01f5765938a8713afc518e2a1d4
Thinh 'Central' Le <thinhmnsd2002@gmail.com>
2024-06-16 11:30:32 +0700

    feat: handle https

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import * as fs from "fs";
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync("./src/cert/key.pem"),
-    cert: fs.readFileSync("./src/cert/cert.pem"),
-  };
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("/api/v1");
 
   const config = new DocumentBuilder()
